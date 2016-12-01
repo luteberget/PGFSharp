a464c68d65396744f1d1c1c147689b50201965bb
Bjørnar Luteberget <bjlut@anacon.no>
2016-12-01 21:08:27 +0100

    Export GF types, unfinished: lacking real Expression object

---
+++ ExportGFTypes/CodeGenerator.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ExportGFTypes
+{
+	public class CodeGenerator
+	{
+		public struct Grammar
+		{
+			public string Name;
+			public List<Category> Categories;
+		}
+
+		public struct Category
+		{
+			public string Name;
+			public List<Constructor> Constructors;
+		}
+
+		public struct Constructor
+		{
+			public string Name;
+			public List<string> ArgumentTypes;
+		}
+
+		//private const string VisitorTypeParam = "R";
+
+		private static string CategoryToCSharpDatatype (string namespc, string name)
+		{
+			// Special cases for built-in types
+			// FIXME: maybe this is not needed -- the C# data types have accidentaly the same names?
+			if (name == "Int")
+				return "int";
+			if (name == "Float")
+				return "double";
+			if (name == "String")
+				return "string";
+			if (name == VISITOR_PARAM)
+				return name;
+
+			return $"{namespc}.{name}";
+		}
+
+		private static IEnumerable<string> VariableNames ()
+		{
+			int i = 1;
+			while (true) {
+				yield return $"x{i++}";
+			}
+		}
+
+		private static string ArgList (string namespc, IEnumerable<string> types)
+		{
+			return String.Join (",", types.Zip (VariableNames (), (t, v) => $"{CategoryToCSharpDatatype(namespc, t)} {v}"));
+		}
+
+		private static string TypeList (string namespc, IEnumerable<string> types) =>
+		String.Join(",", types.Select(s => CategoryToCSharpDatatype(namespc, s)));
+
+		private static string VISITOR_PARAM = "R";
+
+		public static string ToString (Grammar grammar)
+		{
+			var b = new StringBuilder ();
+			Action<string> print = s => b.AppendLine (s);
+
+			using (var namespc = new Bracketed (print, $"namespace {grammar.Name}")) { 
+
+				foreach (var cat in grammar.Categories) {
+					using (var catClass = new Bracketed (namespc.Print, $"public abstract {cat.Name} : Tree")) { 
+						catClass.Print ("public abstract R Accept<R>(IVisitor<R> visitor);");
+
+						using (var visitorInterface = new Bracketed (catClass.Print, "public interface IVisitor<R>")) {
+							foreach (var constr in cat.Constructors) {
+								visitorInterface.Print ($"R Visit{constr.Name}({ArgList(grammar.Name, constr.ArgumentTypes)});");
+							}
+						}
+
+						using (var visitorClass = new Bracketed (catClass.Print, "public class Visitor<R> : IVisitor<R>")) {
+							var constrNames = cat.Constructors.Select (c => c.Name);
+							var argLists = cat.Constructors.Select (c => ArgList (grammar.Name, c.ArgumentTypes));
+							var varLists = cat.Constructors.Select (c => String.Join(", ", VariableNames ().Take (c.ArgumentTypes.Count ())));
+							var lambdaTypes = cat.Constructors.Select (c => $"Func<{TypeList(grammar.Name, c.ArgumentTypes.Concat(new[]{VISITOR_PARAM}))}>");
+							var funcFields = constrNames.Zip (lambdaTypes,
+								                 (name, type) => $"private {type} _Visit{name} {{ get; set; }}");
+							var constructorArgs = String.Join (", ", constrNames.Zip (lambdaTypes,
+								                      (name, type) => $"{type} Visit{name}"));
+
+							var constructorAssignments = constrNames.Select (c => $"this._Visit{c} = Visit{c};");
+							var interfaceImplementationDecl = constrNames.Zip (argLists, 
+								                                   (name, args) => $"public R Visit{name}({args})");
+							var interfaceImplementationBody = constrNames.Zip (varLists,
+								                                   (name, vars) => $"_Visit{name}({vars});");
+							var interfaceImplementaion = interfaceImplementationDecl.Zip (interfaceImplementationBody,
+								                               (decl, body) => $"{decl} => {body}");
+
+							foreach (var s in funcFields)
+								visitorClass.Print (s);
+
+							using (var visitorConstructor = new Bracketed (visitorClass.Print, $"public Visitor({constructorArgs})")) { 
+								foreach (var assign in constructorAssignments)
+									visitorConstructor.Print (assign);
+							}
+
+							foreach (var impl in interfaceImplementaion)
+								visitorClass.Print (impl);
+						}
+					}
+
+					foreach (var constr in cat.Constructors) {
+
+						using (var constrClass = new Bracketed (namespc.Print, $"public class {constr.Name} : {cat.Name}")) { 
+
+							var fields = constr.ArgumentTypes.Zip(VariableNames(), (type, name) => $"public {type} {name} {{get; set;}}");
+							var constructorArgs = String.Join(", ", constr.ArgumentTypes.Zip(VariableNames(), (type, name) => $"{type} {name}"));
+							var vars = VariableNames().Take(constr.ArgumentTypes.Count());
+							var varList = String.Join(", ", vars);
+							var constructorAssignments = vars.Select(name => $"this.{name} = {name};");
+
+							foreach(var f in fields) constrClass.Print(f);
+
+							using(var constrConstructor = new Bracketed(constrClass.Print, $"public {constr.Name}({constructorArgs})")) {
+								foreach(var a in constructorAssignments) constrConstructor.Print(a);
+							}
+
+							using(var visitorAccept = new Bracketed(constrClass.Print, "public override R Accept<R>(IVisitor<R> visitor)")) {
+								visitorAccept.Print($"visitor.Visit{constr.Name}({varList});");
+							}
+						}
+					}
+				}
+			}
+			return b.ToString ();
+		}
+
+		public class Bracketed : IDisposable
+		{
+			Action<string> printer;
+
+			public Bracketed (Action<string> printer, string something)
+			{
+				this.printer = printer;
+				printer (something + " {");
+			}
+
+			public void Print (string s) => printer("  " + s);
+
+			public void Dispose ()
+			{
+				printer ("}");
+				printer ("");
+			}
+		}
+	}
+}
+
---
+++ ExportGFTypes/MyClass.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PGF;
+using ExportGFTypes.Query;
+namespace ExportGFTypes
+{
+
+	// Generate C# classes corresponding to a GF grammar.
+	// Incomplete, does not support:
+	//  - Higher-order and dependent types
+	//  - List types
+
+	public class MyClass
+	{
+		public MyClass ()
+		{
+			//var ans = Query.Answer.FromExpression (Expression.FromString("Yes"));
+			var x = Query.Answer.FromExpression(null);
+
+		}
+	}
+
+	// GF tutorial: putting it all together (answer query function)
+
+
+	public class Application {
+		
+		public static bool IsOdd (int x) => x % 2 == 1;
+		public static bool IsEven (int x) => x % 2 == 0;
+		public static bool IsPrime (int x) => Sieve(Enumerable.Range(2,x)).Contains(x);
+		public static IEnumerable<int> Sieve(IEnumerable<int> xs) => 
+		  !xs.Any() ? Enumerable.Empty<int>() :
+		    xs.Take(1).Concat(Sieve(xs.Skip(1).Where(n => n % xs.First() > 0)));
+
+		static void Main() {
+			string input = null;
+			Expression inputExpr = null; // Parse expression
+			Expression inputExpr2 = null; // Parse concerte language
+
+			Query.Question question = Query.Question.FromExpression (inputExpr);
+
+			// Do something
+
+			Query.Answer answer = Transfer (question);
+			var outputExpr = answer.ToExpression ();
+		}
+
+		/*private class QuestionAnswerVisitor : Query.Question.Visitor<Query.Answer> {
+			public Query.Answer VisitPrime (Query.Object Object0) => Test (IsPrime, Object0);
+			public Query.Answer VisitOdd (Query.Object Object0) => Test (IsOdd, Object0);
+			public Query.Answer VisitEven (Query.Object Object0) => Test (IsEven, Object0);
+		}*/
+
+		private static Answer Transfer(Question q) {
+			//return q.Accept(new QuestionAnswerVisitor ());
+			return q.Accept(new Question.Visitor<Answer>(
+				VisitPrime: obj => Test(IsPrime, obj),
+				VisitOdd: obj => Test(IsOdd, obj),
+				VisitEven: obj => Test(IsEven, obj)
+			));
+		}
+
+		/*private class ObjectNumberVisitor : Query.Object.Visitor<int> {
+			public int VisitNumber (int Int0) => Int0;
+		}*/
+
+		private static int IntValue(Query.Object o) {
+			return o.Accept (new Query.Object.Visitor<int>(i => i));
+		}
+
+		private static Answer Test (Func<int, bool> testF, Query.Object obj) =>
+		  testF(IntValue(obj)) ? (Answer) new Yes() : (Answer) new No();
+
+		/*{
+			if(test(Value(obj))) {
+				return new Yes();
+			} else {
+				return new No();
+			}
+		}*/
+		  //test(Value(obj)) ? (Query.Answer) new Query.Yes() : (Query.Answer) new Query.No();
+	}
+
+/*
+ * 
+ *   abstract Query = {
+
+    flags startcat=Question ;
+
+    cat
+      Answer ; Question ; Object ;
+
+    fun
+      Even   : Object -> Question ;
+      Odd    : Object -> Question ;
+      Prime  : Object -> Question ;
+      Number : Int -> Object ;
+
+      Yes : Answer ;
+      No  : Answer ;
+  }
+*/
+
+	// Each category is a class.
+	// Each 
+
+	public abstract class Tree {
+		
+
+		public abstract string ToExpressionString();
+
+		public Expression ToExpression() {
+			throw new NotImplementedException ();
+			///return ToExpressionString ();
+		}
+
+		public abstract string ClassName { get;}
+
+		// TODO: move to 
+		protected string MkCId(string s) => "(" + s + ")";
+		protected string MkApp(string CId, IEnumerable<string> args)  => "(" + CId + " " + String.Join(" ", args) +  ")";
+	}
+
+	namespace Query {
+		public abstract class Answer : Tree {
+			public abstract R Accept<R>(IVisitor<R> visitor);
+			public interface IVisitor<R> {
+				R VisitYes();
+				R VisitNo();
+			}
+
+			public class Visitor<R> : IVisitor<R>{
+				private Func<R> _VisitYes { get; set; }
+				private Func<R> _VisitNo { get; set; }
+
+
+				public Visitor(Func<R> VisitYes, Func<R> VisitNo) {
+					this._VisitYes = VisitYes;
+					this._VisitNo = VisitNo;
+				}
+
+				public R VisitYes() => _VisitYes();
+				public R VisitNo() => _VisitYes();
+			}
+
+			public static Answer FromExpression(Expression expr) {
+				//var head = expr.UnApp ();
+				string head = "";
+				if (head == "Yes") new Yes ();
+				if (head == "No") new No ();
+				return null;
+			}
+		}
+
+		public class Yes : Answer { 
+
+			public override string ClassName => nameof(Yes);
+			public override string ToExpressionString () => ClassName;
+
+			public override R  Accept<R>(IVisitor<R> visitor) {
+				return visitor.VisitYes();
+			}
+
+		}
+		public class No : Answer {public override string ClassName => nameof(No);
+			public override string ToExpressionString () => ClassName;
+			public override R  Accept<R>(IVisitor<R> visitor) {
+				return visitor.VisitNo();
+			}
+		}
+
+		public abstract class Object : Tree {
+
+			public interface IVisitor<R> {
+				R VisitNumber(int Int0);
+			}
+			public abstract R Accept<R>(IVisitor<R> visitor);
+
+			public static Object FromExpression(Expression expr) {
+				//var unApp = expr.UnApp ();
+				string unApp = null;
+
+				if (unApp == "Number x")
+					return new Number (0);
+
+				throw new ArgumentOutOfRangeException ();
+			}
+
+			public class Visitor<R> : IVisitor<R>{
+				private Func<int,R> _VisitNumber { get; set; }
+
+				public Visitor(Func<int,R> VisitNumber) {
+					this._VisitNumber = VisitNumber;
+				}
+
+				public R VisitNumber(int Int0) => _VisitNumber(Int0);
+			}
+		}
+
+		public class Number : Object {
+			public override string ClassName => nameof(Number);
+
+			public override string ToExpressionString ()
+			{
+				return MkApp(ClassName, new[]{Int0}.Select(x => x.ToString()));
+			}
+
+			public int Int0 { get; set;}
+			public Number(int Int0) {
+				this.Int0 = Int0;
+			}
+
+			public override R Accept<R>(IVisitor<R> visitor) {
+				return visitor.VisitNumber(Int0);
+			}
+		}
+
+		public abstract class Question :Tree {
+			public interface IVisitor<R> {
+				R VisitPrime(Object Object0);
+				R VisitOdd(Object Object0);
+				R VisitEven(Object Object0);
+			}
+			public abstract R Accept<R>(IVisitor<R> visitor);
+
+			public static Question FromExpression(Expression expr) {
+				//var unApp = expr.UnApp ();
+				string unApp = null;
+
+				if (unApp == "Prime x")
+					return new Prime ( Number.FromExpression(null) );
+				if (unApp == "Odd x")
+					return new Odd (Number.FromExpression (null));
+				if (unApp == "Even x")
+					return new Even (Number.FromExpression (null));
+
+				throw new ArgumentOutOfRangeException ();
+			}
+
+			public class Visitor<R> : IVisitor<R>{
+				private Func<Object,R> _VisitPrime { get; set; }
+				private Func<Object,R> _VisitOdd { get; set; }
+				private Func<Object,R> _VisitEven { get; set; }
+
+				public Visitor(
+					Func<Object,R> VisitPrime,
+					Func<Object,R> VisitOdd,
+					Func<Object,R> VisitEven
+				) {
+					this._VisitPrime = VisitPrime;
+					this._VisitOdd = VisitOdd;
+					this._VisitEven = VisitEven;
+				}
+
+				public R VisitPrime(Object Object0) => _VisitPrime(Object0);
+				public R VisitOdd(Object Object0) => _VisitOdd(Object0);
+				public R VisitEven(Object Object0) => _VisitEven(Object0);
+			}
+		}
+
+		public class Prime :Question {
+			public override string ClassName => nameof(Prime);
+			public override string ToExpressionString ()
+			{
+				return MkApp(nameof(Prime), new[]{Object0}.Select(x => x.ToExpressionString()));
+			}
+
+			public Object Object0 {get;set;}
+			public Prime(Object Object0) { this.Object0 = Object0;}
+			public override R Accept<R>(IVisitor<R> visitor) {
+				return visitor.VisitPrime(Object0);
+			}
+		}
+
+		public class Odd :Question {
+			public override string ClassName => nameof(Odd );
+			public override string ToExpressionString ()
+			{
+				return MkApp(ClassName, new[]{Object0}.Select(x => x.ToExpressionString()));
+			}
+			public Object Object0 {get;set;}
+			public Odd(Object Object0) { this.Object0 = Object0;}
+			public override R Accept<R>(IVisitor<R> visitor) {
+				return visitor.VisitOdd(Object0);
+			}
+		}
+
+		public class Even :Question {
+			public override string ClassName => nameof(Even);
+			public override string ToExpressionString ()
+			{
+				return MkApp(ClassName, new[]{Object0}.Select(x => x.ToExpressionString()));
+			}
+			public Object Object0 {get;set;}
+			public Even(Object Object0) { this.Object0 = Object0;}
+			public override R Accept<R>(IVisitor<R> visitor) {
+				return visitor.VisitEven(Object0);
+			}
+		}
+	}
+}
+
